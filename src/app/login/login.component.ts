5098856e29674d047066acdfb770bb921115f8d1
Jose correa <jose.correa-r@mail.escuelaing.edu.co>
2024-03-06 17:14:16 -0500

    creacion de front login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor() { }
+
+  login() {
+    // Aquí puedes implementar la lógica de autenticación
+    console.log('Usuario:', this.username);
+    console.log('Contraseña:', this.password);
+    // Por ejemplo, podrías hacer una solicitud HTTP a tu backend para autenticar al usuario
+  }
 }
+
